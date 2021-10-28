22d2719f5f819b3e5700a1fa6d6f698ab943c173
alfredochs <chanta.alfredo@gmail.com>
2021-10-28 09:59:29 +0200

    commenti

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 
-alertNumbersRandom();
+/* ------------------------------------------------------------------------------------------------------------------------------*/
+/**
+ * Genera Numeri Random tra 1 e 100
+ */
+function generateRandomNum(minNumber = 1, maxNumber = 20) {
+    const numRandom = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    return numRandom;
+}
 
-let conteggioSecondi = 0;
-const intervallo = setInterval(() => {
-    console.log(`intervallo di 1 secondo per ${conteggioSecondi + 1} volte`);
-    conteggioSecondi++;
-    if (conteggioSecondi >= 30) {
-        clearInterval(intervallo);
-    }
-}, 1000);
+/* ------------------------------------------------------------------------------------------------------------------------------*/
+// let conteggioSecondi = 0;
+// const intervallo = setInterval(() => {
+//     console.log(`intervallo di 1 secondo per ${conteggioSecondi + 1} volte`);
+//     conteggioSecondi++;
+//     if (conteggioSecondi >= 30) {
+//         clearInterval(intervallo);
+//     }
+// }, 1000);
 
 /*
 crea un intervallo di 30 secondi in questo caso con nome const = Intervallo
@@ -17,59 +25,48 @@ secIntervallo aggiunge di uno per non partire da 0.
 dentro l'if si setta il tempo che deve trascorrere per pulire l'intervallo
 */
 
-// const clockInterval2 = setInterval(() => {
-//     console.log("log eseguito dopo x tempo", clockInterval2);
-// }, 1500);
-
-// console.log(clockInterval);
-// console.log(clockInterval2);
-
-// setTimeout(() => console.log("questo è il primo timeout"), 1000);
-// setTimeout(() => console.log("questo è il secondo timeout"), 2000);
-// setTimeout(() => console.log("questo è il terzo timeout"), 3000);
-// setTimeout(() => console.log("questo è il quarto timeout"), 4000);
+//1 Un alert() espone 5 numeri generati casualmente.
+//2 Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l’utente deve inserire uno alla volta, i numeri che ha visto precedentemente, 
+// tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri
+// da indovinare sono stati individuati.
 
 
-
-// Un alert() espone 5 numeri generati casualmente.
-// Da li parte un timer di 30 secondi.
-// Dopo 30 secondi l’utente deve inserire uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-
-// Consigli del giorno:
-// * Pensate prima in italiano.
-// * Dividete in piccoli problemi la consegna.
-// * Individuate gli elementi di cui avete bisogno per realizzare il programma.
-
-
-/* 
-mostro l'alert con i numeri random da ricordare
-una volta nascosto l'alert faccio partire i 30 secondi in cui devo inserire i numeri random da ricordare
-
+/*
+numeri casuali = una funzione
+dentro l'alert se sono x numeri random, fare un ciclo che andra a inserire x numeri dentro l'alert
+devo usare il se timeout che inizierà il conteggio fino a 30 secondi
+    dentro il timeout si dovrà usare il prompt per 5 volte per inserire i numeri da ricordare dell'alert
+    questi numeri dovranno essere inseriti dentro un contenitore per poi paragonarli col contenitore dell'alert
 */
 
-/* ------------------------------------------------------------------------------------------------------------------------------*/
-/**
- * Genera Numeri Random tra 1 e 100
- * @returns 
- */
-function generateRandomNum(minNumber = 1, maxNumber = 100) {
-    const numRandom = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-    return numRandom;
-}
+let numeriAlert = [];
+let inputContainer = [];
+let confrontoArrays = [];
 
-/**
- * genera l'alert con 5 numeri random diversi. Se li avessi assegnati ad una costante il numero random sarebbe lo stesso per tutti e 5
- */
-function alertNumbersRandom() {
-    alert(`
-    ${generateRandomNum()}
-    ${generateRandomNum()}
-    ${generateRandomNum()}
-    ${generateRandomNum()}
-    ${generateRandomNum()}`);
+while (numeriAlert.length < 3) {
+    const numeroAlert = generateRandomNum();
+    numeriAlert.push(numeroAlert);
 }
+console.log(`I numeri dell'alert sono ${numeriAlert}`);
 
-function promptGenerator() {
-    prompt("Inserisci i numeri dell'alert: ");
-}
+alert(numeriAlert);
+
+const timer = setTimeout(function () {
+
+    while (inputContainer < 3) {
+        const numeriInput = prompt("Inserisci i numeri dell'alert: ");
+        let numeriPerArray = numeriInput.split(" ");
+        inputContainer.push(numeriPerArray);
+    }
+    console.log(`Array di numeri inseriti dall'utente ${inputContainer}`);
+    for (let i = 0; i < inputContainer.length; i++) {
+        const numeroInput = inputContainer[i];
+
+        if (numeriAlert.includes(numeroInput)) {
+            confrontoArrays.push(numeroInput);
+        }
+    }
+
+}, 1000);
